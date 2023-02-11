c7cb71616071bc9d553f045d571f5022ae63c20d
Shrivats <shrivatskrishna25@gmail.com>
2023-02-11 11:21:25 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {console.log(document.location.host)}
         <a
           className="App-link"
           href="https://reactjs.org"
