8e45ebb7b4786c2c0ad945086d56a1dc4a8f6791
Aniket Mishra <aniketmishra2709@gmail.com>
2023-02-11 14:53:00 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+/*global chrome*/
 function App() {
+  
+  chrome.webRequest.onHeadersReceived.addListener(
+    function(details) {
+      // Get the Content-Length header
+
+      const contentLengthHeader = details.responseHeaders.find(header => header.name.toLowerCase() === 'content-length');
+      if (contentLengthHeader) {
+        // Calculate the size of the response body
+        const responseSize = parseInt(contentLengthHeader.value);
+        
+        // Add the data transferred to the total
+        setDataTransfer(localStorage.getItem(details.url) + responseSize);
+        setUrl(details.url);
+        localStorage.setItem(details.url, responseSize);
+        // Log the data transferred for this request
+        console.log(`Data transferred for ${details.url}: ${totalDataTransferred} bytes`);
+      }
+    },
+    {urls: ["<all_urls>"]},
+    ["responseHeaders"]
+  );
+  
+
+  // Add a listener for the browser action to display the total data transferred
+  chrome.browserAction.onClicked.addListener(function (tab) {
+    // Display the total data transferred in a popup
+    alert(`Total data transferred: ${totalDataTransferred} bytes`);
+  });
   return (
-    
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {console.log(document.location.host)}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{url} - {totalDataTransferred}</h1>
     </div>
   );
 }
